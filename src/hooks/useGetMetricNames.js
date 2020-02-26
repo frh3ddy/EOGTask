9d4f0cd2d9682e0dcf56aee503b842ba0c3b398c
Fredy Mendez <fmendez@NSLFMENDEZsMBP.nearsoft-general.net>
2020-02-26 14:54:47 -0700

    create useGetMetricNames to retrieve the list of metrics

---
+++ src/hooks/useGetMetricNames.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { actions } from '../MetricsSelector/reducer';
+import { useQuery } from 'urql';
+
+const query = `
+    {
+        getMetrics
+    }
+`;
+
+const useGetMetricNames = () => {
+  const [result] = useQuery({
+    query,
+  });
+
+  const metrics = useSelector((state: any) => state.metrics.metricsNames);
+
+  const dispatch = useDispatch();
+  const { fetching, data } = result;
+
+  useEffect(() => {
+    if (!fetching) {
+      dispatch(actions.metricsNamesRetreaved(data));
+    }
+  }, [fetching]);
+
+  return metrics;
+};
+
+export default useGetMetricNames;
