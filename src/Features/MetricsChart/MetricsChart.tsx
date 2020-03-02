920e1f8737411bc761d3f8eb9398ac828e0ba938
Fredy Mendez <fmendez@NSLFMENDEZsMBP.nearsoft-general.net>
2020-03-02 13:15:59 -0700

    make chart container responsive

--- src/Features/MetricsChart/MetricsChart.tsx
+++ src/Features/MetricsChart/MetricsChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import useGetMultipleMesurements from '../../hooks/useGetMultipleMesurements';
 
 const colors: string[] = ['#f75176', '#10acf3', '#dd2ade', '#525174', '#fece42', '#1ba362'];
@@ -13,37 +13,39 @@ const Chart = () => {
 
   if (selectedMetricsMetadata.length > 0) {
     return (
-      <LineChart
-        width={1200}
-        height={500}
-        data={[...measurements]}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="at" interval="preserveStartEnd" minTickGap={20} tickFormatter={formatDateToTime} />
-        {selectedMetricsMetadata.map((metric: { [key: string]: string; unit: string }, index: number) => (
-          <Line
-            isAnimationActive={false}
-            key={metric.name}
-            dot={false}
-            yAxisId={metric.unit}
-            name={metric.name}
-            type="linear"
-            dataKey={metric.name}
-            stroke={colors[index]}
-          />
-        ))}
-        {metricUnits.map((unit: string, index: number) => (
-          <YAxis key={index} yAxisId={unit} label={{ value: unit, position: 'insideBottomLeft' }} />
-        ))}
-        <Tooltip labelFormatter={value => new Date(value).toDateString()} />
-        <Legend />
-      </LineChart>
+      <ResponsiveContainer width={'100%'} height={600}>
+        <LineChart
+          width={1200}
+          height={500}
+          data={[...measurements]}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="at" interval="preserveStartEnd" minTickGap={20} tickFormatter={formatDateToTime} />
+          {selectedMetricsMetadata.map((metric: { [key: string]: string; unit: string }, index: number) => (
+            <Line
+              isAnimationActive={false}
+              key={metric.name}
+              dot={false}
+              yAxisId={metric.unit}
+              name={metric.name}
+              type="linear"
+              dataKey={metric.name}
+              stroke={colors[index]}
+            />
+          ))}
+          {metricUnits.map((unit: string, index: number) => (
+            <YAxis key={index} yAxisId={unit} label={{ value: unit, position: 'insideBottomLeft' }} />
+          ))}
+          <Tooltip labelFormatter={value => new Date(value).toDateString()} />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
     );
   } else {
     return null;
