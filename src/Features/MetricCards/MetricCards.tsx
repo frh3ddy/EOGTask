c4f382d854a83114e4247261b093711a91971c52
Fredy Mendez <fmendez@NSLFMENDEZsMBP.nearsoft-general.net>
2020-03-02 15:19:33 -0700

    call stopPropagation on chip delete event

--- src/Features/MetricCards/MetricCards.tsx
+++ src/Features/MetricCards/MetricCards.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import GridList from '@material-ui/core/GridList';
 import Typography from '@material-ui/core/Typography';
 import useMetricsData from '../../hooks/useMetricsData';
 import { Measurement } from './reducer';
 
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      marginBottom: 20,
-    },
-    gridList: {
-      flexWrap: 'nowrap',
-      transform: 'translateZ(0)',
+      flexGrow: 1,
+      margin: 25,
     },
     card: {
       minWidth: 250,
@@ -26,29 +20,31 @@ const useStyles = makeStyles(() =>
   }),
 );
 
-const SimpleCard = () => {
-  const { metricsData, selectedMetrics } = useMetricsData();
+export default function MetricCards() {
   const classes = useStyles();
-  const data = Object.values<Measurement>(metricsData).filter((g: Measurement) => selectedMetrics.includes(g.metric));
+  const { metricsData, selectedMetrics } = useMetricsData();
+  const data = Object.values<Measurement>(metricsData).filter((measurement: Measurement) =>
+    selectedMetrics.includes(measurement.metric),
+  );
 
   return (
     <div className={classes.root}>
-      <GridList className={classes.gridList} cols={2.5} cellHeight={120}>
-        {data.map(mesurement => (
-          <Card className={classes.card} key={mesurement.metric}>
-            <CardContent>
-              <Typography variant="h5" component="h1">
-                {mesurement.metric}
-              </Typography>
-              <Typography variant="h2" component="h1">
-                {mesurement.value}
-              </Typography>
-            </CardContent>
-          </Card>
+      <Grid container spacing={3}>
+        {data.map(measurement => (
+          <Grid item xl={2} key={measurement.metric}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h1">
+                  {measurement.metric}
+                </Typography>
+                <Typography variant="h2" component="h1">
+                  {measurement.value}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
-      </GridList>
+      </Grid>
     </div>
   );
-};
-
-export default SimpleCard;
+}
